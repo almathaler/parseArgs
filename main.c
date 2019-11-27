9c64baa012785e197e909df00d52e144285a5dec
almathaler <athaler00@stuy.edu>
2019-11-26 22:47:52 -0500

    no print statements

--- main.c
+++ main.c
@@ -16,7 +16,6 @@ int main(){
   //now should have one long string
   //printf("here is input: %s\n", input);
   char **args = parse_args(input);
-  printf("here are the args: args[0]: %s\t args[1]: %s\t args[2]: %s\n", args[0], args[1], args[2]);
   execvp(args[0], args);
   printf("done!\n");
   free(args);
@@ -26,14 +25,10 @@ int main(){
 char ** parse_args(char * line){
   char **to_return = malloc(5 * sizeof(char *)); //check
   int i = 0;
-  printf("in parse_args, here is line: %s\n", line);
   while(*line != '\0'){ //this will make the last arg null by default bc strsep returns null when it's done
     to_return[i] = strsep(&line, " \t\n");
-    printf("line at %d: %s\n", i, line);
-    printf("to_return[%d]: %s\n", i, to_return[i]);
     i++;
   }
   to_return[i] = NULL;
-  printf("in parse_args, size of i: %d\n", i);
   return to_return;
 }
